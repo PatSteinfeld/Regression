d86022ec65a59548e8a2fd9236737d9757bbf622
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-02-04 18:06:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -54,7 +54,7 @@ if uploaded_file:
 
             df["Category"] = df["Date Difference"].apply(categorize_days)
             # Convert 'Category' into an ordered categorical type
-            CATEGORY_ORDER = ["0-30 days", "31-60 days", "61-90 days", "91-180 days", "180+ days", "N/A"]
+            CATEGORY_ORDER = ["0-30 days", "31-60 days", "61-90 days", "91-180 days", "More then 180 days", "N/A"]
             
             # Convert 'Category' into an ordered categorical type
             df["Category"] = pd.Categorical(df["Category"].astype(str).str.strip(), categories=CATEGORY_ORDER, ordered=True)
