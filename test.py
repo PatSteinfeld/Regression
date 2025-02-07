30e4dcd7442540f18a0ea8b9a851c8306eaadd69
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-02-07 16:33:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -67,10 +67,10 @@ if uploaded_file:
 
             # Grouping data for visualization
             rcc = df.groupby(['Category', 'RC Type']) \
-                    .agg({'Split Man-Days': 'sum', 'Project Planner': lambda x: list(set(x))}) \
+                    .agg({'Split Man-Days': 'sum', ['Project Planner','Project Responsible']: lambda x: list(set(x))}) \
                     .reset_index()
 
-            rcc.columns = ['Category', 'RC Type',  'Man-Days', 'Project Planner']
+            rcc.columns = ['Category', 'RC Type',  'Man-Days', 'Project Planner','Project Responsible']
 
             # Dropdown for selecting category
             selected_category = st.selectbox("Select a Category", ["All"] + list(rcc["Category"].unique()))
