9e05baba437d80e4f0b5a5ca9a11a3da758845fc
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-03-05 16:08:57 +0530

    Delete 📂 auditors-planning-app/📄 app.py

--- 📂 auditors-planning-app/📄 app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Auditors Planning App", layout="wide")
-
-st.title("Auditors Planning Schedule App")
-st.sidebar.success("Select a page above.")
-st.write("Use the sidebar to navigate between pages.")
