d9a2879accfbec81e84432133045ef25a9166c8f
PatSteinfeld <110217700+PatSteinfeld@users.noreply.github.com>
2025-03-05 16:13:07 +0530

    Create app.py

---
+++ 📂 auditors-planning-app/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Auditors Planning App", layout="wide")
+
+st.title("Auditors Planning Schedule App")
+st.sidebar.success("Select a page above.")
+st.write("Use the sidebar to navigate between pages.")
